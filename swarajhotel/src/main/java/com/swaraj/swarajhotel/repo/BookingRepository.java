cbbfa992e1512faf5779232f26cb12c49d93376a
Pranavbc <pranav1952000@gmail.com>
2025-01-27 17:43:47 +0530

    commit4

--- swarajhotel/src/main/java/com/swaraj/swarajhotel/repo/BookingRepository.java
+++ swarajhotel/src/main/java/com/swaraj/swarajhotel/repo/BookingRepository.java
@@ -1,4 +1,15 @@
 package com.swaraj.swarajhotel.repo;
 
-public interface BookingRepository {
+import com.swaraj.swarajhotel.entity.Booking;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface BookingRepository extends JpaRepository<Booking , Long> {
+
+    List<Booking> findByRoomId(Long roomId);
+
+    List<Booking> findByBookingConfirmationCode(String confirmationCode);
+
+    List<Booking> findByUserId(Long userId);
 }
